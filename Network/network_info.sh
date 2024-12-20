f703e5c04504f01402efcce1dc6ba00681bba96a
Mohamed Tamer <mt0991787@gmail.com>
2024-12-20 23:23:30 +0000

    Finished Network Info Script

--- Network/network_info.sh
+++ Network/network_info.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-echo "Network info is here"
-X=$(whoami)
-Y=$(pwd)
-Z=$(git --version)
-zenity --info --title="Network" --text="$X \n $Y \n $Z"
+echo "Fetching Network Details..."
+recieved=$(ifconfig enp0s3 | grep -i 'RX p'| awk '{print $3 " packets / " $5 " bytes"}')
+transmitted=$(ifconfig enp0s3 | grep -i 'TX p'| awk '{print $3 " packets / " $5 " bytes"}')
+dropped=$(ifconfig enp0s3 | grep -i 'TX e'| awk '{print $5 " packets"}')
+zenity --info --title="Network" --text="Recieved = $recieved\nTransmitted = $transmitted\nDropped = $dropped"
