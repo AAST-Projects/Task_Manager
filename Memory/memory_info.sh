7265b9f65125d364044ffa8f428e510a5f85401b
Mohamed Tamer <mt0991787@gmail.com>
2024-12-20 21:56:35 +0000

    Finished the Memory Info script

--- Memory/memory_info.sh
+++ Memory/memory_info.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
-echo "Mmeory is 8 GB"
-X=$(pwd)
-Y=$(python3 --version)
-Z=$(whoami)
-zenity --info --title="Memory" --text="Memory Capacity is $X GB\nMemory Utilization is $Y %\nI am $Z"
+echo "Fetching Memory details"
+total_memory=$(free -m | awk '/Mem:/ {print $2 " MB"}')
+used_memory=$(free -m | awk '/Mem:/ {print $3 " MB"}')
+free_memory=$(free -m | awk '/Mem:/ {print $4 " MB"}')
+cached_memory=$(free -m | awk '/Mem:/ {print $6 " MB"}')
+available_memory=$(free -m | awk '/Mem:/ {print $7 " MB"}')
+
+zenity --info --title="Memory" --text="Total Memory = $total_memory\nUsed Memory = $used_memory\nFree Memory = $free_memory\nCached Memory = $cached_memory\nAvailable Memory = $available_memory"
